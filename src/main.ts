c1d9c5f986c22f61e8a9b557fee29ebfea9ea58a
2tle <iam@2tle.io>
2022-01-20 17:52:05 +0900

    fix: script npm run dev bug

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import express, {Request, Response, NextFunction} from "express";
 const app = express();
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is listening port 3000");
+    console.log("Server is listening port 3000!");
 })
 
 
