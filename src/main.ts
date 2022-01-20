8331e492b5ea12f9c14e01528a3dc25884dd3d55
2tle <iam@2tle.io>
2022-01-20 15:37:03 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import express, {Request, Response, NextFunction} from "express";
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is listening port 3000");
+})
+
+
